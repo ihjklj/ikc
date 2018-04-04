e19837d8c0c10d1a50eafebc27e1b92011cf3243
ihjklj <750477889@qq.com>
2018-04-04 17:03:32 +0800

    update

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,13 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := ikc
+LOCAL_MODULE := test
 LOCAL_C_INCLUDES := $(LOCAL_PATH) $(NDK)/sources/cxx-stl/stlport/stlport
 LOCAL_SRC_FILES := ik_native.cpp \
 					ik_network.cpp \
 					ik_thread.cpp \
-					ik_util.cpp \
-					ik_function.cpp 
+					ik_function.cpp \
+					ik_util.cpp 
 LOCAL_CFLAGS := -static
 LOCAL_LDLIBS := -pthread -dl
 LOCAL_LDFLAGS := -L. -lc -lstdc++ -llog
@@ -16,7 +16,7 @@ LOCAL_LDLIBS := -L. -lc -llog -ldl -lz
 //LOCAL_CFLAGS += -pie -fPIE
 //LOCAL_LDFLAGS += -pie -fPIE
 
-#������������ϵ�����ⲻ����ʱ��ȥ��������⡣
+#会生成依赖关系，当库不存在时会去编译这个库。
 #LOCAL_SHARED_LIBRARIES 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_EXECUTABLE)
