c7e7bd25dd864a484880e55395403b13090dab61
ihjklj <750477889@qq.com>
2018-04-04 14:39:50 +0800

    update

---
+++ app/src/main/java/com/ihjklj/ikc/fragment/OwnFragment.java
@@ -0,0 +1,21 @@
+package com.ihjklj.ikc.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2018/4/4.
+ */
+
+public class OwnFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = null;
+        return view;
+    }
+}
---
+++ app/src/main/java/com/ihjklj/ikc/fragment/VillageAffairFragment.java
@@ -0,0 +1,21 @@
+package com.ihjklj.ikc.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2018/4/4.
+ */
+
+public class VillageAffairFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle savedInstanceState) {
+        View view = null;
+        return view;
+    }
+}
